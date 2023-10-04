18da40fc9d6765e10f1bee4b8152d0388a5cbffd
Kishan Lal <kannoujiya7309@gmail.com>
2023-10-04 21:03:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `please give a valid height:${height}`;
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `please give a valid weight:${weight}`;
+    }
+    else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        //show the result
+        //results.innerHTML=`<span>${bmi}</span>`;
+
+        if (bmi < 18.6) {
+            results.innerHTML = `BMI is=${bmi},You are underweight`;
+
+        }
+        else if (bmi > 18.6 && bmi < 24.9) {
+            results.innerHTML = `BMI is=${bmi},You are in normal range`;
+
+        }
+        else {
+            results.innerHTML = `BMI is=${bmi},you are overweight`;
+        }
+    }
+})
